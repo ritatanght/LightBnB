77b8140a8fd54b855947a38c5e853a5bbc0c2938
Rita Tang <doggymint@gmail.com>
2023-09-20 22:33:03 -0400

    Revise getAllReservations function to fetch from postgres

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -74,7 +74,22 @@ const addUser = function(user) {
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  //return getAllProperties(null, 2);
+  const query = `
+    SELECT properties.*, reservations.*, AVG(rating) as average_rating
+    FROM properties
+    JOIN reservations ON properties.id = reservations.property_id
+    JOIN property_reviews ON reservations.property_id = property_reviews.property_id
+    WHERE reservations.guest_id = $1
+    GROUP BY properties.id, reservations.id
+    ORDER BY start_date
+    LIMIT $2;
+  `;
+
+  return pool
+    .query(query, [guest_id, limit])
+    .then((res) => res.rows)
+    .catch((err) => console.log(err.message));
 };
 
 /// Properties
